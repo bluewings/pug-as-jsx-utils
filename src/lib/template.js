1e25615c1b4dab75ac0a477d163a1c5f0d63c4a6
sungwon.cha <mygenie3@naver.com>
2019-01-18 10:06:57 +0900

    bugfix

--- src/lib/template.js
+++ src/lib/template.js
@@ -3,9 +3,13 @@ const macro = `const __macro = {
     map: (mapFn) => {
       let mapFns = [];
       if (items && items['@@__IMMUTABLE_MAP__@@']) {
-        items.mapEntries(([key, value], i) => mapFns.push(mapFn(value, key, i)));
+        items.mapEntries(([key, value], i) => {
+          mapFns.push(mapFn(value, key, i));
+        });
       } else if (items && items['@@__IMMUTABLE_LIST__@@']) {
-        items.forEach((value, i) => mapFns.push(mapFn(value, i, i)));
+        items.forEach((value, i) => {
+          mapFns.push(mapFn(value, i, i));
+        });
       } else if (Array.isArray(items)) {
         mapFns = items.map((value, index) => mapFn(value, index, index));
       } else {
